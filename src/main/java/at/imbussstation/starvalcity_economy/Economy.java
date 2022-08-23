1b2c514ac869ca26fc2b3f616499964df547371f
Imbussstation <63212799+Imbussstation@users.noreply.github.com>
2022-08-23 15:24:02 +0200

    First commit

---
+++ src/main/java/at/imbussstation/starvalcity_economy/Economy.java
@@ -0,0 +1,17 @@
+package at.imbussstation.starvalcity_economy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Economy extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
