48340d59efeb2d40356050bfef5d9b7e4ba19157
Nirflector <nirflector@gmail.com>
2022-10-17 17:20:17 +0200

    Project Creation

---
+++ src/main/java/de/starvalcity/starvaleconomy/Core.java
@@ -0,0 +1,17 @@
+package de.starvalcity.starvaleconomy;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Core extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
